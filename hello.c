518ffe061d3222711bbb55a6581ee235abcf1242
Rafael Flores <yosoyrafh@proton.me>
2026-01-28 18:24:12 -0500

    Add hello.c to print greeting message

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // Standard Input-Output library
+
+int main() {
+    // printf displays text on the screen
+    printf("Hello, C world!\n");
+    
+    // Return 0 tells the system the program finished successfully
+    return 0;
+}
